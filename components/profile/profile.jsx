9c013b9199589afb18bc818e06102b48719a0a83
StepanovaOk <143267396+StepanovaOk@users.noreply.github.com>
2024-03-15 23:46:00 +0200

    1

--- components/profile/profile.jsx
+++ components/profile/profile.jsx
@@ -1,26 +1,27 @@
 import PropTypes from "prop-types";
+import styles from "./profile.module.css";
 
 const Profile = ({ name, tag, location, image, stats }) => (
-  <div>
-    <div>
-      <img src={image} alt="User avatar" />
-      <p>{name}</p>
-      <p>@{tag}</p>
-      <p>{location}</p>
+  <div className={styles.card}>
+    <div className={styles.description}>
+      <img className={styles.img} src={image} alt="User avatar" />
+      <p className={styles.name}>{name}</p>
+      <p className={styles.tag}>@{tag}</p>
+      <p className={styles.location}>{location}</p>
     </div>
 
-    <ul>
-      <li>
-        <span>Followers</span>
-        <span>{stats.followers}</span>
+    <ul className={styles.list}>
+      <li className={styles.item}>
+        <span className={styles.text}>Followers</span>
+        <span className={styles.number}>{stats.followers}</span>
       </li>
-      <li>
-        <span>Views</span>
-        <span>{stats.views}</span>
+      <li className={styles.item}>
+        <span className={styles.text}>Views</span>
+        <span className={styles.number}>{stats.views}</span>
       </li>
-      <li>
-        <span>Likes</span>
-        <span>{stats.likes}</span>
+      <li className={styles.item}>
+        <span className={styles.text}>Likes</span>
+        <span className={styles.number}>{stats.likes}</span>
       </li>
     </ul>
   </div>
